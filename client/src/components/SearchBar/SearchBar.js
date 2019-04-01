e64e33df597806334a53a7fc54a7691a6bd29271
Adam Cooper <Adam.Cooper@ynap.com>
2019-04-01 17:20:02 +0100

    floating search bar

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import './SearchBar.css';
+import React from "react";
+import "./SearchBar.css";
 
-export const SearchBar = (props) => {
-
+export const SearchBar = props => {
   return (
-    <div className='container'>
-    <input className='searchbar' ref={props.getNode} onChange={props.onChange} placeholder='Search...' />
-    <div className='searchbarButton' onClick={props.onClickButton} />
-    <span className='line' />
+    <div className="searchBarWrapper">
+      <input
+        className="searchbar"
+        ref={props.getNode}
+        onChange={props.onChange}
+        placeholder="Search..."
+      />
+      <div className="searchbarButton" onClick={props.onClickButton} />
+      <span className="line" />
     </div>
-  )
-}
+  );
+};
 
 export default SearchBar;
