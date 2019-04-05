3e8a9ef90dc91fa0c5f1d02ff9a916ca5d5c4f2d
Adam Cooper <Adam.Cooper@ynap.com>
2019-04-05 13:13:08 +0100

    fix travis error

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import MainContainer from './containers/MainContainer';
-
-require('matchmedia-polyfill');
-require('matchmedia-polyfill/matchMedia.addListener');
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MainContainer from "./containers/MainContainer";
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
+        <div className="App">
           <div>
             <Route exact path="/" component={MainContainer} />
           </div>
-      </div>
+        </div>
       </Router>
     );
   }
