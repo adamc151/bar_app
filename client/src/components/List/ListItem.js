210412f3cb3509141e73b410d551fc9eed322136
Tom Morris <thomasmorris823@gmail.com>
2019-06-27 13:30:32 +0100

    list item styling

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ListItem.css";
 import locationIcon from "./placeholder.png";
+import barImg from "./bar.jpg";
 
 export default function ListItem(props) {
   const { onHover = () => {}, onClick = () => {}, data, className } = props;
@@ -15,22 +16,13 @@ export default function ListItem(props) {
         onHover(props.data);
       }}
     >
-    {<img src={locationIcon} className="titleIcon" alt="" />}
-    {name && <div className="itemName">{name}</div>} 
-      {deals[0].category && <div className="itemLocation">{deals[0].category}</div>}
-      {deals[0].description && (
-        <div className="itemDescription">{deals[0].description}</div>
-      )}
-      {deals[0].endTime && (
-        <div className="itemLocation">
-          {deals[0].startTime} - {deals[0].endTime}
-        </div>
-      )}
-      {website && (
-        <div className="itemLocation">
-          <a href={website}>Website</a>
-        </div>
-      )}
+      {<img src={barImg} className="barImg" alt="" />}
+      {/* {<img src={locationIcon} className="titleIcon" alt="" />} */}
+      {name && <div className="itemName">{name}</div>} 
+      {deals[0].category && <div className={ `item${deals[0].category} itemCategory`}>{deals[0].category}</div>}
+      {deals[0].description && (<div className="itemDescription">{deals[0].description}</div>)}
+      {deals[0].endTime && (<div className="itemLocation">{deals[0].startTime} - {deals[0].endTime}</div>)}
+      {website && (<div className="itemLocation"><a href={website}>Website</a></div>)}
     </div>
   );
 }
