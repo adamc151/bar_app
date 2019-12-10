3c6a3a2de7741d905ffbb10781effd1b21464262
Tom Morris <thomasmorris823@gmail.com>
2019-12-10 16:03:33 +0000

    Added orange background to landing page

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -56,49 +56,49 @@ render() {
 
             <img className="descriptionLandingItemFullLogo" src={fullLogo}/>
 
-            <div className="descriptionAndTitleLandingWrapper">
-                <div className="descriptionLandingTitle">HOW IT WORKS</div>
-                <div className="descriptionLandingWrapper">
-                    <div className="descriptionLandingItem">
-                        <img className="descriptionLandingItemImg" src={location}/>
-                        <div className="descriptionLandingText">
-                            <div className="descriptionLandingItemTitle">FIND YOUR LOCATION</div>
-                            <div className="descriptionLandingItemDesc">Search for an area you want to see hapi hours for or provide your current location.</div>
-                        </div>
-                    </div>
-                    <div className="descriptionLandingItem">
-                        <img className="descriptionLandingItemImg" src={calendar}/>
-                        <div className="descriptionLandingText">
-                            <div className="descriptionLandingItemTitle">SEE TODAY'S HAPI HOURS</div>
-                            <div className="descriptionLandingItemDesc">See our active & upcoming hapi hours for today on the map.</div>
-                        </div>
-                    </div>
-                    <div className="descriptionLandingItem">
-                        <img className="descriptionLandingItemImg" src={beers}/>
-                        <div className="descriptionLandingText">
-                            <div className="descriptionLandingItemTitle">PLAN YOUR NIGHT</div>
-                            <div className="descriptionLandingItemDesc">See currently active happy hours as well as those about to start so you can plan your next move!</div>
-                        </div>
-                    </div>
-                </div>
+            <div className="descriptionColourWrapper">
+              <div className="descriptionAndTitleLandingWrapper">
+                  <div className="descriptionLandingTitle">HOW IT WORKS</div>
+                  <div className="descriptionLandingWrapper">
+                      <div className="descriptionLandingItem">
+                          <img className="descriptionLandingItemImg" src={location}/>
+                          <div className="descriptionLandingText">
+                              <div className="descriptionLandingItemTitle">FIND YOUR LOCATION</div>
+                              <div className="descriptionLandingItemDesc">Search for an area you want to see hapi hours for or provide your current location.</div>
+                          </div>
+                      </div>
+                      <div className="descriptionLandingItem">
+                          <img className="descriptionLandingItemImg" src={calendar}/>
+                          <div className="descriptionLandingText">
+                              <div className="descriptionLandingItemTitle">SEE TODAY'S HAPI HOURS</div>
+                              <div className="descriptionLandingItemDesc">See our active & upcoming hapi hours for today on the map.</div>
+                          </div>
+                      </div>
+                      <div className="descriptionLandingItem">
+                          <img className="descriptionLandingItemImg" src={beers}/>
+                          <div className="descriptionLandingText">
+                              <div className="descriptionLandingItemTitle">PLAN YOUR NIGHT</div>
+                              <div className="descriptionLandingItemDesc">See currently active happy hours as well as those about to start so you can plan your next move!</div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
             </div>
 
 
             <div className="donationWrapper">
                   <div className="donationItem">
-                  {<div className="line">--</div>}
+                  {/* {<div className="line">--</div>} */}
                       <div className="">
                           <div className="donationItemTitle">BUY US A ROUND? :)</div>
                           <div className="donationItemDesc">Hapihour is a free, self-funded product. If you think hapihour is useful, maybe buy us a coffee/beer to keep us going?</div>
                       </div>
                       <a href={'https://paypal.me/hapihour/3.5'}><img className="" src={'https://img.shields.io/badge/Donate-PayPal-orange.svg'}/></a>
-                  {/* {<div className="line">--</div>} */}
                   </div>
             </div>
 
             <div className="footerCenter">
                 <div className="faqFooter" onClick={() => {history.push(`/faq`);}}><a>FAQ</a></div>
-                {/* <div className="footerItemTitle">CONTACT US</div> */}
                 <a href="https://twitter.com/hapihour_io" className="inline"><img src={twitterIcon}/></a>
                 <a className="inline dot">•</a>
                 <a href="https://www.instagram.com/hapihour.io/" className="inline"><img src={instagramIcon}/></a>
