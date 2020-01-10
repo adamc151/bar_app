1af7e6da9bf23fb9dbf2560fdd6a3cd3789e1211
Adam Cooper <Adam.Cooper@ynap.com>
2020-01-10 16:41:21 +0000

    fixesyo

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -25,9 +25,9 @@ const ListItem = (props) => {
       {name && <div className="itemName"><img src={locationIcon} className="titleIconInside" alt="" />{name}</div>}
       {deals[0].category && <div className={`item${deals[0].category} itemCategory`}>{deals[0].category}</div>}
       {deals[0].description && (<div className="itemDescription">{deals[0].description.join(' • ')}</div>)}
-      {deals[0].category === "Now" && deals[0].endTime && (<div className="itemTime">Ends at {deals[0].endTime}</div>)}
-      {deals[0].category === "Upcoming" && deals[0].endTime && (<div className="itemTime">Starts at {deals[0].startTime}</div>)}
-      {deals[0].category === "Inactive" && deals[0].endTime && (<div className="itemTime">Finished at {deals[0].endTime}</div>)}
+      {deals[0].category == "Now" && deals[0].endTime && (<div className="itemTime">Ends at {deals[0].endTime}</div>)}
+      {deals[0].category == "Upcoming" && deals[0].endTime && (<div className="itemTime">Starts at {deals[0].startTime}</div>)}
+      {deals[0].category == "Inactive" && deals[0].endTime && (<div className="itemTime">Finished at {deals[0].endTime}</div>)}
     </div>
   ) : (
       <div className={`listItemWrapper carouselCard`}>
--- client/src/components/Place/Place.js
+++ client/src/components/Place/Place.js
@@ -53,9 +53,9 @@ class Place extends React.Component {
         }
 
         let calcWeekDays = [];
-        for (var z = 0, l = weekDays.length; z < l; z++) {
-          if (weekDays[z].selected) {
-            calcWeekDays.push(parseInt(weekDays[z].value, 10));
+        for (var i = 0, l = weekDays.length; i < l; i++) {
+          if (weekDays[i].selected) {
+            calcWeekDays.push(parseInt(weekDays[i].value));
           }
         }
 
