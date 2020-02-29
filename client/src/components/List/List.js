ffcb96c2f952aead7025e3e93216c67c5f693609
Tom Morris <thomasmorris823@gmail.com>
2020-02-29 17:07:24 +0000

    Change logic to put inactive deals at the end of the displayed list

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -3,8 +3,19 @@ import ListItem from "./ListItem";
 import "./List.css";
 
 const getList = (data, setSingleBar, onHover) => {
+
   if (data.length > 0) {
-    return data.map((data, i) => {
+
+    let tmpInactiveData =[];
+    let tmpActiveData =[];
+
+    data.map((item, i) =>{
+      item.deals[0] && item.deals[0].category=='Inactive' ? tmpInactiveData.push(item) : tmpActiveData.push(item);
+    });
+
+    const tmpFinalData = tmpActiveData.concat(tmpInactiveData);
+
+    return tmpFinalData.map((data, i) => {
       return <ListItem
         key={i}
         index={i}
