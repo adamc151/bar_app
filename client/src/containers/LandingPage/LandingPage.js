26838ba0003bfb5f266a8b26d7675d9bdb721360
Tom Morris <thomasmorris823@gmail.com>
2020-06-11 14:16:07 +0100

    General styling updates

--- client/src/containers/LandingPage/LandingPage.js
+++ client/src/containers/LandingPage/LandingPage.js
@@ -3,21 +3,18 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../../state/actions/actions";
 import "./LandingPage.css";
+import { Route, Link } from "react-router-dom";
+import { Parallax, Background } from "react-parallax";
 
 import mobileHeader from "../images/tom_hh_2.jpg";
-import desktopHeader from "../images/background_img2.jpg";
+import desktopHeader from "../images/group_1.jpg";
 import locationIcon from "../icons/placeholder.png";
-import twitterIcon from "../icons/twitter.png";
-import emailIcon from "../icons/email.png";
-import instagramIcon from "../icons/instagram.png";
-import facebookIcon from "../icons/facebook.png";
-import { Route, Link } from "react-router-dom";
 import Image from "../../components/Image/Image";
-import { Parallax, Background } from "react-parallax";
 import Preview from "./Preview/Preview";
 import HowItWorks from "./HowItWorks/HowItWorks";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Donation from "./Donation/Donation";
 
 class LandingPage extends Component {
   componentDidMount() {
@@ -84,32 +81,8 @@ class LandingPage extends Component {
 
             <Preview />
             <HowItWorks />
-            <Link to={`/map`}>
-              <button className="CheckItOutButton">Check it Out</button>
-            </Link>
-
-            <div className="donationWrapper">
-              <div className="donationItem">
-                <div className="">
-                  <div className="donationItemTitle">Buy us a round?</div>
-                  <div className="donationItemDesc">
-                    Hapihour is a free, self-funded service. If you think
-                    hapihour is useful, maybe buy us a coffee/beer to keep us
-                    going?
-                  </div>
-                </div>
-                <a href={"https://paypal.me/hapihour/3.5"}>
-                  <img
-                    alt="Paypal Donation"
-                    className=""
-                    src={
-                      "https://img.shields.io/badge/Donate-PayPal-orange.svg"
-                    }
-                  />
-                </a>
-              </div>
-            </div>
-
+            <Link to={`/map`}> <button className="CheckItOutButton">Check it Out</button></Link>
+            <Donation />
             <Footer />
           </div>
         )}
