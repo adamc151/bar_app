19077edd31e0846f70f016e09f8421af50a94e5d
Tom Morris <thomasmorris823@gmail.com>
2020-06-20 12:44:05 +0100

    General styling

--- client/src/containers/SellingPage/SellingPage.js
+++ client/src/containers/SellingPage/SellingPage.js
@@ -3,24 +3,41 @@ import "./SellingPage.css";
 import { Route, Link } from "react-router-dom";
 import Header from "../LandingPage/Header/Header";
 import Footer from "../LandingPage/Footer/Footer";
-import Image from "../../components/Image/Image";
 import AdminPreview from "../images/admin_preview.png";
 
 class SellingPage extends Component {
-
   render() {
-
     return (
       <Route
         render={({ history }) => (
           <div className="SellingPage">
             <Header history={history} />
             <div className="SellingPageTopBlock">
-              <div className="SellingPageTopBlockTitle">Make your happy hour deals stand out amoungst the noise.</div>
-              <div className="SellingPageTopBlockMicroTitle">We feature your deals so that customers can see what YOU have to offer.</div>
-              <button className="">Sign Up For A Spot</button>
+              <div className="SellingPageTopBlockTitle">
+                Make your happy hour deals stand out amoungst the noise.
+              </div>
+              <div className="SellingPageTopBlockMicroTitle">
+              <b style={{fontFamily: 'Circular-Black', fontSize: '36px'}}>Sign up</b> to manage your details and we will feature <b style={{fontFamily: 'Circular-Black', fontSize: '36px'}}>your</b> deals so that customers can see what <b style={{fontFamily: 'Circular-Black', fontSize: '36px'}}>you</b> have to
+                offer.
+              </div>
+              <button className="">Get Access To The Admin Centre</button>
             </div>
-            <img className="SellingPagePreviewImage" src={AdminPreview}/>
+
+            <img className="SellingPagePreviewImage" src={AdminPreview} />
+
+            <div className="SellingPageProblem">
+              <h1>Own a bar or resturant and want featured listings?</h1>
+              <p>
+                Describe your customer’s biggest, most expensive pain point. Use
+                the same words they use when talking about it. Drive it home
+                with emotion.
+              </p>
+            </div>
+
+            <div className="SellingPageCallToAction">
+              <h1>Remind them why they came</h1>
+              <button className="">Request Featured Listing</button>
+            </div>
             <Footer />
           </div>
         )}
