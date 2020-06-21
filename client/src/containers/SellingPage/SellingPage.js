9c0ac31c381ba706124f9c9cbe7cc0e2a0051d69
Tom Morris <thomasmorris823@gmail.com>
2020-06-21 13:20:16 +0100

    Add theme colouring to header

--- client/src/containers/SellingPage/SellingPage.js
+++ client/src/containers/SellingPage/SellingPage.js
@@ -11,22 +11,33 @@ class SellingPage extends Component {
       <Route
         render={({ history }) => (
           <div className="SellingPage">
-            <Header history={history} />
+            <Header history={history} theme="Black" />
             <div className="SellingPageTopBlock">
               <div className="SellingPageTopBlockTitle">
                 Make your happy hour deals stand out amoungst the noise.
               </div>
               <div className="SellingPageTopBlockMicroTitle">
-              <b style={{fontFamily: 'Circular-Black', fontSize: '36px'}}>Sign up</b> to manage your details and we will feature <b style={{fontFamily: 'Circular-Black', fontSize: '36px'}}>your</b> deals so that customers can see what <b style={{fontFamily: 'Circular-Black', fontSize: '36px'}}>you</b> have to
-                offer.
+                <b style={{ fontFamily: "Circular-Black", fontSize: "30px" }}>
+                  Sign up
+                </b>{" "}
+                to manage your details and we will feature{" "}
+                <b style={{ fontFamily: "Circular-Black", fontSize: "30px" }}>
+                  your
+                </b>{" "}
+                deals so that customers can see what{" "}
+                <b style={{ fontFamily: "Circular-Black", fontSize: "30px" }}>
+                  you
+                </b>{" "}
+                have to offer.
               </div>
               <button className="">Get Access To The Admin Centre</button>
+              <p>Free earlybird access • No credit card required</p>
             </div>
 
             <img className="SellingPagePreviewImage" src={AdminPreview} />
 
             <div className="SellingPageProblem">
-              <h1>Own a bar or resturant and want featured listings?</h1>
+              <h1>Own a bar and want featured listings?</h1>
               <p>
                 Describe your customer’s biggest, most expensive pain point. Use
                 the same words they use when talking about it. Drive it home
