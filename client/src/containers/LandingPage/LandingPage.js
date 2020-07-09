0bd49a19c55fa84a97775b5321556cbbcbc8ffa9
Adam Cooper <adam.cooper@ynap.com>
2020-07-09 17:03:37 +0100

    testimonials

--- client/src/containers/LandingPage/LandingPage.js
+++ client/src/containers/LandingPage/LandingPage.js
@@ -15,6 +15,7 @@ import HowItWorks from "./HowItWorks/HowItWorks";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Donation from "./Donation/Donation";
+import Testimonials from "./Testimonials/Testimonials";
 
 class LandingPage extends Component {
   componentDidMount() {
@@ -98,6 +99,7 @@ class LandingPage extends Component {
             <Preview />
             <HowItWorks />
             {/* <Preview /> */}
+            <Testimonials />
             {barSellingBanner}
             {/* <Link to={`/map`}>{" "}<button className="CheckItOutButton">Try For Free</button></Link> */}
             {/* <Donation /> */}
