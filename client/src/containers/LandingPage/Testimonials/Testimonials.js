a2555c875dc449e6a28a8aa1bde1ea08d88420f2
Adam Cooper <adam.cooper@ynap.com>
2020-07-09 17:03:37 +0100

    testimonials

---
+++ client/src/containers/LandingPage/Testimonials/Testimonials.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Slider from "react-slick";
+import "./Testimonials.css";
+import testimonials from "./testimonials.json";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div className={"nextArrowWrapper"} onClick={onClick}>
+      <div class="arrow-right" />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div className={"previousArrowWrapper"} onClick={onClick}>
+      <div class="arrow-left" />
+    </div>
+  );
+}
+
+const getTestimonials = () => {
+  return testimonials.map(({ name, image, text }) => {
+    return (
+      <div className="testimonialItemWrapper">
+        <img className="testimonialItemImage" src={image} />
+        <div className="testimonialItemName">{name}</div>
+        <div className="testimonialItemText">{text}</div>
+      </div>
+    );
+  });
+};
+
+const Testimonials = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    className: "center",
+    centerMode: true,
+    centerPadding: "20px",
+    initialSlide: 2,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const testimonials = getTestimonials();
+
+  return testimonials.length && false ? (
+    <div className="TestimonialsBackground">
+      <div className="TestimonialsWrapper">
+        <Slider {...settings}>{testimonials}</Slider>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Testimonials;
