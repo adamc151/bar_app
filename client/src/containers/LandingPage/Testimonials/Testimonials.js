06541df6a20981e4656ba811d414ecbf3ce8a0ac
Tom Morris <thomasmorris823@gmail.com>
2020-07-11 20:14:39 +0100

    Added selling page styling and updated images on testimonials

--- client/src/containers/LandingPage/Testimonials/Testimonials.js
+++ client/src/containers/LandingPage/Testimonials/Testimonials.js
@@ -1,7 +1,37 @@
 import React from "react";
 import Slider from "react-slick";
 import "./Testimonials.css";
-import testimonials from "./testimonials.json";
+import tom from '../../images/tom_1.jpg';
+import becki from '../../images/becki_1.jpg';
+import adam from '../../images/tom_1.jpg';
+import ria from '../../images/tom_1.jpg';
+
+const testimonials = [
+  {
+    "image": "https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg",
+    "name": "Adam",
+    "city": "London",
+    "text": "\"When someone is visiting my city and asks me for recommendations on where to grab a drink I just send them to Hapihour as they have all the information right there.\""
+  },
+  {
+    "image": tom,
+    "name": "Tom",
+    "city": "Leeds",
+    "text": "\"I discovered 3 new bars in Leeds because of this website.\""
+  },
+  {
+    "image": "https://pratimasharma.com.np/wp-content/uploads/2019/04/bitmoji-20190410013747-1.png",
+    "name": "Ria",
+    "city": "London",
+    "text": "\"When I finish work I often want to grab a drink somewhere near the office. This website is great for finding the happy hours that are on now or will be starting soon close by.\""
+  },
+  {
+    "image": becki,
+    "name": "Becki",
+    "city": "Leeds",
+    "text": "\"I like how easy the website is to navigate and how they keep up to date information on all the bars in my city. \""
+  }
+];
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
