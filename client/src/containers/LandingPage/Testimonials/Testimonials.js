966e921bd5104e58b575b9bf1646b2774b5adfbc
Tom Morris <thomasmorris823@gmail.com>
2020-07-12 12:06:24 +0100

    Added selling page styling and updated images on testimonials

--- client/src/containers/LandingPage/Testimonials/Testimonials.js
+++ client/src/containers/LandingPage/Testimonials/Testimonials.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Slider from "react-slick";
 import "./Testimonials.css";
-import tom from '../../images/tom_1.jpg';
-import becki from '../../images/becki_1.jpg';
-import adam from '../../images/tom_1.jpg';
-import ria from '../../images/tom_1.jpg';
+import tom from '../../images/tom.jpg';
+import becki from '../../images/becki.jpg';
+import adam from '../../images/adam.jpg';
+import ria from '../../images/ria.jpg';
 
 const testimonials = [
   {
-    "image": "https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg",
+    "image": adam,
     "name": "Adam",
     "city": "London",
     "text": "\"When someone is visiting my city and asks me for recommendations on where to grab a drink I just send them to Hapihour as they have all the information right there.\""
@@ -20,7 +20,7 @@ const testimonials = [
     "text": "\"I discovered 3 new bars in Leeds because of this website.\""
   },
   {
-    "image": "https://pratimasharma.com.np/wp-content/uploads/2019/04/bitmoji-20190410013747-1.png",
+    "image": ria,
     "name": "Ria",
     "city": "London",
     "text": "\"When I finish work I often want to grab a drink somewhere near the office. This website is great for finding the happy hours that are on now or will be starting soon close by.\""
@@ -55,7 +55,7 @@ const getTestimonials = () => {
   return testimonials.map(({ name, image, city, text }) => {
     return (
       <div className="testimonialItemWrapper">
-        <img className="testimonialItemImage" src={image} />
+        <img className="testimonialItemImage" src={image} alt="user testimonial"/>
         <div className="testimonialItemName">{name}</div>
         <div className="testimonialItemCity">{city}</div>
         <div className="testimonialItemText">{text}</div>
